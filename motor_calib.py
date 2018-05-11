873e0679314bf2bbcbaf88c2a7b9e64a2eee5d51
psychomugs <psychomugs@gmail.com>
2018-05-11 19:42:04 -0400

    Added more prompts to motor_calib.py

--- motor_calib.py
+++ motor_calib.py
@@ -12,6 +12,8 @@ if (len(motor_id)>1):
     quit()
 motor_id = motor_id[0]
 
+print("Motor "+str(motor_id)+" found!")
+
 # set motor to 100
 motors.set_goal_position({motor_id:100})
 raw_input("Motor position: 100; Attach horn then press 'Enter'. ")
@@ -27,5 +29,6 @@ print("Motor position: 100; Calibration complete!")
 # change ID
 new_id = int(raw_input("Enter motor ID (1-3 for towers, 4 for base, 5+ for etc): "))
 motors.change_id({motor_id:new_id})
+print("Motor ID changed from "+str(motor_id)+" to "+str(new_id))
 
 quit()
